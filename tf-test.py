32225b0a531d14a19e2afe7670578a824ab5857b
zoeyangyy <zoeyangyy@163.com>
2017-10-01 14:57:04 +0800

    2017/10/01

--- tf-test.py
+++ tf-test.py
@@ -12,8 +12,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from sklearn.datasets import load_digits
-from sklearn.cross_validation import train_test_split
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
 
 
 def test1():
@@ -140,16 +140,14 @@ def test5():
                 plt.pause(0.3)
         writer.close()
 
-# test5()
-
 
 def test6():
     def compute_accuracy(v_xs, v_ys):
         global prediction
-        y_pre = sess.run(prediction, feed_dict={xs:v_xs})
-        correct_prediction = tf.equal(tf.argmax(y_pre, 1),tf.argmax(v_ys, 1))
+        y_pre = sess.run(prediction, feed_dict={xs: v_xs})
+        correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        result = sess.run(accuracy, feed_dict={xs:v_xs,ys:v_ys})
+        result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
         return result
 
     mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
@@ -167,3 +165,79 @@ def test6():
             sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
             if i%50 == 0:
                 print(compute_accuracy(mnist.test.images, mnist.test.labels))
+
+
+def test7():
+    def add_layer(inputs, in_size, out_size, layer_name, activation_function=None):
+        # add one more layer and return the output of this layer
+        Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+        biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, )
+        Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        # here to dropout
+        Wx_plus_b = tf.nn.dropout(Wx_plus_b, keep_prob)
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b, )
+        tf.summary.histogram(layer_name + '/outputs', outputs)
+        return outputs
+
+    digits = load_digits()
+    X = digits.data
+    y = digits.target
+    y = LabelBinarizer().fit_transform(y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3)
+
+    keep_prob = tf.placeholder(tf.float32)
+    xs = tf.placeholder(tf.float32, [None, 64])  # 8x8
+    ys = tf.placeholder(tf.float32, [None, 10])
+
+    # add output layer
+    l1 = add_layer(xs, 64, 50, 'l1', activation_function=tf.nn.tanh)
+    prediction = add_layer(l1, 50, 10, 'l2', activation_function=tf.nn.softmax)
+    # loss
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))
+    tf.summary.scalar('loss', cross_entropy)
+
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+    with tf.Session() as sess:
+        merged = tf.summary.merge_all()
+        # summary writer goes in here
+        train_writer = tf.summary.FileWriter("logs/train", sess.graph)
+        test_writer = tf.summary.FileWriter("logs/test", sess.graph)
+
+        sess.run(tf.global_variables_initializer())
+        for i in range(1000):
+            sess.run(train_step, feed_dict={xs: X_train, ys: y_train, keep_prob: 0.5})
+            if i % 50 == 0:
+                # record loss
+                train_result = sess.run(merged, feed_dict={xs: X_train, ys: y_train, keep_prob: 1})
+                test_result = sess.run(merged, feed_dict={xs: X_test, ys: y_test, keep_prob: 1})
+                train_writer.add_summary(train_result, i)
+                test_writer.add_summary(test_result, i)
+
+
+def test8_saver():
+    W = tf.Variable([[1, 2, 3], [3, 4, 5]], dtype=tf.float32, name='weights')
+    b = tf.Variable([[1, 2, 3]], dtype=tf.float32, name='biases')
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(init)
+        save_path = saver.save(sess, 'my_net/save_net.ckpt')
+        print("save to path:", save_path)
+
+
+def test9_load():
+    W = tf.Variable(np.arange(6).reshape((2, 3)), dtype=tf.float32, name='weights')
+    b = tf.Variable(np.arange(3).reshape((1, 3)), dtype=tf.float32, name='biases')
+
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        saver.restore(sess, 'my_net/save_net.ckpt')
+        print("weight:", sess.run(W))
+        print("biases:", sess.run(b))
+
